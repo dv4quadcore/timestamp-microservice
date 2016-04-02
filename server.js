b5ff7e07b1102b287a346e1104b8d1ce47c81843
dvicente04 <dvicente04@alumni.depaul.edu>
2016-04-01 20:24:52 -0500

    port change for heroku

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
 routes(app);
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000...');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port...');
 });
